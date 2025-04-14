414deb14086b3e89ddbc55ce03435f396dd92399
ryuker <lifeisagamenotaproduct@gmail.com>
2025-04-14 17:08:47 +0200

    modified post import type

--- project/AstroWPHeadless/src/content/types/post.ts
+++ project/AstroWPHeadless/src/content/types/post.ts
@@ -2,6 +2,6 @@ import { z } from "astro:content";
 import type { gqlPostSchema, restPostSchema } from "../schemas/post";
 
 
-export type Post = z.infer<typeof restPostSchema>;
+export type RestPost = z.infer<typeof restPostSchema>;
 
 export type GQLPost = z.infer<typeof gqlPostSchema>;
